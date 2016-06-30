bc7c4ce77d320defcaba7686119cb63f58e86a6e
Bahattin Cinic <bahattin.cinic@metglobal.com>
2016-06-30 11:37:26 +0300

    add unicode literals for porting python 3

--- django_onesky/client/base.py
+++ django_onesky/client/base.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import requests
 import time
 import os
--- django_onesky/status.py
+++ django_onesky/status.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 """
 Descriptive HTTP status codes, for code readability.
 See RFC 2616 - http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
